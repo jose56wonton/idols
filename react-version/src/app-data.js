d41b4fc15cb6cf1dc0339df53d80dcbd76dc5bf4
Joshua Wootonn <joshuawootonn@gmail.com>
2018-04-02 23:38:09 -0500

    Finished color shifting

---
+++ react-version/src/app-data.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    title: "Idols",
+    sub: "from bondage to freedom"
+  },
+  {
+    title: "Parents",
+    sub: "2004-2012"
+  },
+
+  {
+    title: "Pleasure",
+    sub: "2012-2017"
+  },
+  {
+    title: "Relationship",
+    sub: "2016-2017"
+  },
+  {
+    title: "Success",
+    sub: "2017-2018"
+  },
+  {
+    title: "Significance",
+    sub: "2018"
+  },
+  {
+    title: "Next",
+    sub: "-"
+  }
+];
